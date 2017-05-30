de901466c24227d7d3031db17ea23c5efc4c03ce
Jake <jdai54@gmail.com>
2017-05-30 15:12:35 -0700

    Assignment 12: Complete

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get 'welcome/index'
 
   get 'welcome/about'
+
+  get 'welcome/faq'
   # 'root' declares the default page the app loads when we navigate to the home page URL. "root" is a method that takes a hash as an argument. here we use implied hash syntax instead of: root{{to: 'welcome#index'}}
   root 'welcome#index'
 
