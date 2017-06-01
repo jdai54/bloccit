41625ac930923bc3f05172b9ca4fe43d0f69e673
Jake <jdai54@gmail.com>
2017-05-31 21:54:10 -0700

    Checkpoint 16: Complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'random_data'
+
+# Create posts
+50.times do
+  # #1 use "create!" with a bang which instructs the method to raise an error if there's a problem with the data being seeded. Using "create" without a bang coudl fail without warning causing the error to surface later
+  Post.create!(
+  # #2 the RandomData method will create random strings for "title" and "body"
+    title: RandomData.random_sentence,
+    body: RandomData. random_paragraph
+  )
+end
+posts = Post.all
+
+# Create comments
+# #3 call "times' a specified number of times based on the Integer. "
+100.times do
+  Comment.create!(
+  # #4 call "sample" on the array returned by "Post.all" in order to pick a random post to associate with each comment. "sample" returns a random element from the array every time it's called
+    post: posts.sample,
+    body: RandomData.random_paragraph
+  )
+end
+
+puts "Seed Finished"
+puts "#{Post.count} posts created"
+puts "#{Comment.count} comments created"
---
+++ lib/random_data.rb
@@ -0,0 +1,30 @@
+# #5 define "RandomData" as a module because it is a standalone library with no dependencies or inheritance requirements. Modules can be used as mixins to add functions to multiple classes
+module RandomData
+  # #6 define "random_paragraph" and set "sentences" to an array. We create four to six random sentences and append them to "sentences". We call "join" on "sentences" to combine each sentence in the array, passing a space to spearate each one
+  def self.random_paragraph
+    sentences = []
+    rand(4..6).times do
+      sentences << random_sentence
+    end
+
+    sentences.join(" ")
+  end
+
+  # #7 follow the same pattern as in #6 to create a sentence with a random number of words. After a sentence is generated, we call "capitalize" on it and append a period
+  def self.random_sentence
+    strings = []
+    rand(3..8).times do
+      strings << random_word
+    end
+
+    sentence = strings.join(" ")
+    sentence.capitalize << "."
+  end
+
+  # #8 define "random_word". Set "letters" to an array of letters a through z using to_a. Call shuffle! on "letters" in place using the bang. Join the zeroth through nth item in "letters". The nth item is the result of rand(3..8) which returns a random number greater than or equal to three and less than eight
+  def self.random_word
+    letters = ('a'..'z').to_a
+    letters.shuffle!
+    letters[0,rand(3..8)].join
+  end
+end
