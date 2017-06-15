52a7613861e157a245159b71bc0cede9c8d0ca2d
Jake <jdai54@gmail.com>
2017-06-15 12:43:07 -0700

    Checkpoint 23: Complete

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
     # #34 pass "resources :posts" to the "resources :topics" block. This nests the post routes under the topic routes
     resources :posts, except: [:index]
   end
+
+  # #7
+  resources :users, only: [:new, :create]
   # #2 we remove "get "welcome/index" because we've declared the index view as the root view. We also modify the "about" route to allow users to visit "/about" rather than "/welcome/about"
   get 'about' => 'welcome#about'
   # 'root' declares the default page the app loads when we navigate to the home page URL. "root" is a method that takes a hash as an argument. here we use implied hash syntax instead of: root{{to: 'welcome#index'}}
