e4212d5013b6966bb4c4df61fccb9071b1a50299
Jake <jdai54@gmail.com>
2017-06-16 14:53:35 -0700

    Assignment 24: Complete

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   # #7
   resources :users, only: [:new, :create]
   # #2 we remove "get "welcome/index" because we've declared the index view as the root view. We also modify the "about" route to allow users to visit "/about" rather than "/welcome/about"
+  post 'users/confirm' => 'users#confirm'
   get 'about' => 'welcome#about'
   # 'root' declares the default page the app loads when we navigate to the home page URL. "root" is a method that takes a hash as an argument. here we use implied hash syntax instead of: root{{to: 'welcome#index'}}
   root 'welcome#index'
