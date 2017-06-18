59cfdc80fc46b9ddfb33e25981df10ff131bc52f
Jake <jdai54@gmail.com>
2017-06-17 17:54:46 -0700

    Assignment 25: Complete

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,9 @@ module ApplicationHelper
   # #5 the content_tag helper method is called. This method is used to build the HTML and CSS to display the form element and any associated errors. Helpers are written in Ruby and usually return HTML. It takes a symbol argument, a block, and an options hash. It then creates a symbol-specified HTML tag with the block contents, and if specified, the options
     content_tag :div, capture(&block), class: css_class
   end
+
+  def avatar_url(user)
+  gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+  "http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
+  end
 end
